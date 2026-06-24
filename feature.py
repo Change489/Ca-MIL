ad4383be59bbe65ea7f3a88fef1b358176a2c100
Change489 <55897617+Change489@users.noreply.github.com>
2026-06-24 17:19:46 +0800

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,4 +1,3 @@
-
 import torch
 from kmeans_pytorch import kmeans
 from config import NUM_CLUSTERS, DEVICE
@@ -10,41 +9,38 @@ def extract_instance_features(model, data_loader):
     FC_feature: [num_bags, window, ROI, ROI]
     instance_features: [num_instances, feature_dim]
     bag_labels: [num_bags]
-    loss_consistency
+    loss_ent
     """
     model.eval()
     FC_list = []
     instance_level_list = []
     label_list = []
-    loss_consistency_total = 0
+    loss_ent_total = 0
 
     with torch.no_grad():
         for data, labels in data_loader:
             data, labels = data.to(DEVICE), labels.to(DEVICE)
 
-            # 卷积和注意力提取 FC
-            FC, consistency_loss = model.extract_features(data)
+            FC, ent_loss = model.extract_features(data)
 
             m_batchsize, m_channel, m_sliding, width, _ = FC.size()
 
-            # 重塑为实例级矩阵
             instance_mats = FC.view(m_batchsize * m_sliding, width, width)
 
-            # 上三角向量
             upper = model.upper_triangular(instance_mats)
 
             FC_list.append(FC)
             instance_level_list.append(upper)
             label_list.append(labels)
-            loss_consistency_total += consistency_loss
+            loss_ent_total += ent_loss
 
-    loss_consistency_mean = loss_consistency_total / len(data_loader)
+    loss_ent_mean = loss_ent_total / len(data_loader)
 
     bag_labels = torch.cat(label_list, dim=0)
     FC_feature = torch.cat(FC_list, dim=0).squeeze(1)
     instance_feature = torch.cat(instance_level_list, dim=0).squeeze(1)
 
-    return FC_feature, instance_feature, bag_labels, loss_consistency_mean
+    return FC_feature, instance_feature, bag_labels, loss_ent_mean
 
 
 def run_kmeans(instance_features):
