ca1629603087dda39ce761a3a99e3961e28646c7
Change489 <55897617+Change489@users.noreply.github.com>
2026-06-24 17:22:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import torch.optim as optim
 from config import DEVICE, EPOCHS, LR
 from feature import extract_instance_features, run_kmeans
 
-
 def train(model, train_loader):
 
     optimizer = optim.Adam(model.parameters(), lr = LR)
@@ -11,8 +10,7 @@ def train(model, train_loader):
     for epoch in range(EPOCHS):
         model.train()
 
-
-        FC_feature, instance_feature, bag_labels, loss_consistency = \
+        FC_feature, instance_feature, bag_labels, loss_ent = \
             extract_instance_features(model, train_loader)
 
         cluster_centers = run_kmeans(instance_feature)
@@ -20,8 +18,7 @@ def train(model, train_loader):
         pred, Y_hat = model(FC_feature.to(DEVICE), cluster_centers)
 
         bce = torch.nn.BCELoss()(pred, bag_labels.float().to(DEVICE))
-        loss = bce + loss_consistency
-
+        loss = bce + loss_ent
 
         optimizer.zero_grad()
         loss.backward()
